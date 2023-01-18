1d90c510fb71845df0eb67bd85af2ce5067187a0
Fernandez Lautaro <fernandez.lautaro@hotmail.com>
2023-01-17 21:17:23 -0300

    agregando toggle menu, todavia no funciona bien

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -1,3 +1,21 @@
 const altoNavegacion = document.querySelector('.navegacion').offsetHeight;
 
-document.documentElement.style.setProperty('--scroll-padding',altoNavegacion + 'px')
+document.documentElement.style.setProperty('--scroll-padding',altoNavegacion + 'px');
+
+const navToggle = document.querySelector(".toggle");
+const links = document.querySelector(".item-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click",() =>{
+    navMenu.classList.toggle("nav-menu_visible");
+    if (navMenu.classList.contains("nav-menu_visible")){
+        navToggle.setAttribute("aria-label","Cerrar menú");
+    }else {
+        navToggle.setAttribute("aria-label","Abrir menú");
+    }
+});
+
+links.addEventListener("click",() =>{
+    navMenu.classList.remove("nav-menu_visible");
+});
+
